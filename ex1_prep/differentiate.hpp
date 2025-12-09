b2e4eecc0a8b5e61a6c18ec31aa4fee004e734c9
Marta Wleklinska <wleklinskam@gmail.com>
2025-12-09 16:59:42 +0100

    update gitignore

--- ex1_prep/differentiate.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<iostream>
-class Differential {
-    public:
-        double func(double t, double y);
-        double euler_step(double t, double y, double h);
-        double rk4_step(double t, double y, double h);
-};
